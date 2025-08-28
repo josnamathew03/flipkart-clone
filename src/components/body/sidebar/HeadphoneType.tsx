076eadce55f26fe6e5024db72fcd4f97270a9e1d
Josna Mathew <150210764+josnamathew03@users.noreply.github.com>
2025-08-28 23:16:15 +0530

    feat: sidebar partial

--- src/components/body/sidebar/HeadphoneType.tsx
+++ src/components/body/sidebar/HeadphoneType.tsx
@@ -2,8 +2,14 @@
 const HeadphoneType = () => {
   return (
     <div className="price-filter-con ">
-      <div className="selected-heading-wrap">
-        <div className="selected-heading selected-heading-small headphone-head">Headphone Type</div>
+      <div className="selected-heading-wrap headphone-head">
+        <div className="selected-heading selected-heading-small ">Headphone Type</div>
+        <svg width="16" height="27" viewBox="0 0 16 27" xmlns="http://www.w3.org/2000/svg" ><path d="M16 23.207L6.11 13.161 16 3.093 12.955 0 0 13.161l12.955 13.161z" fill="#212121" ></path></svg>
+      </div>
+      <div className="type-selection-con">
+        <div>
+            
+        </div>
       </div>
     </div>
   )
