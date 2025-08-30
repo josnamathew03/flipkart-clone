2cce9c01f8b25cd3859ee87f14af55b0af9584d6
Josna Mathew <150210764+josnamathew03@users.noreply.github.com>
2025-08-30 09:00:26 +0530

    feat: footer 1 contents added

--- src/components/footer/footer1/Footer1.tsx
+++ src/components/footer/footer1/Footer1.tsx
@@ -4,7 +4,23 @@ const Footer1 = () => {
   return (
     <div className="footer1-con">
       <div className="footer1-wrap">
-        <div className="footer1"></div>
+        <div className="footer1">
+            <a href="/">
+            SAMSUNG Headset ,Ambrane Headset ,E-calorie Headset ,Philips Headset ,FD1 Headset ,DIZO Headset ,TCL Headset ,Bluetooth & Wired Headset ,Sennheiser Headset ,Skullcandy Headset ,WeCool Headset ,True Wireless Headset ,ROQ Headset ,Apple Headset ,Wefuse Headset ,realme Headset ,Toshiba Headset ,Motorola Headset ,Grostar Headset ,SYARA Headset
+            </a>
+            <h2>Buy Headphones Online at Flipkart</h2>
+            &nbsp; &nbsp;
+            <p>Headphones have become a must-have gadget in today's world owing to the need of listening to music . It is important to buy these headphones from a reliable online store as there are too many defective and duplicate products being sold. Known for customer friendly policies, Flipkart is one website that you can rely on to buy the best headphones.
+                 <br></br>
+            <br></br>
+            
+Check out new launches: 
+            </p>
+      
+            <p></p>
+  
+
+        </div>
       </div>
     </div>
   )
