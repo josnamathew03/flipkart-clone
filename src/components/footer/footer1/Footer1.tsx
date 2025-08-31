327d2c20d1a3e9c13d067f1090062a3332008cb1
Josna Mathew <150210764+josnamathew03@users.noreply.github.com>
2025-08-31 23:57:09 +0530

    feat: footer1 contents added

--- src/components/footer/footer1/Footer1.tsx
+++ src/components/footer/footer1/Footer1.tsx
@@ -1,25 +1,50 @@
-
+import '../Footer.css'
 
 const Footer1 = () => {
   return (
-    <div className="footer1-con">
-      <div className="footer1-wrap">
-        <div className="footer1">
-            <a href="/">
-            SAMSUNG Headset ,Ambrane Headset ,E-calorie Headset ,Philips Headset ,FD1 Headset ,DIZO Headset ,TCL Headset ,Bluetooth & Wired Headset ,Sennheiser Headset ,Skullcandy Headset ,WeCool Headset ,True Wireless Headset ,ROQ Headset ,Apple Headset ,Wefuse Headset ,realme Headset ,Toshiba Headset ,Motorola Headset ,Grostar Headset ,SYARA Headset
-            </a>
-            <h2>Buy Headphones Online at Flipkart</h2>
-            &nbsp; &nbsp;
-            <p>Headphones have become a must-have gadget in today's world owing to the need of listening to music . It is important to buy these headphones from a reliable online store as there are too many defective and duplicate products being sold. Known for customer friendly policies, Flipkart is one website that you can rely on to buy the best headphones.
-                 <br></br>
+    <div className='footer1-con'>
+      <div className='footer1-wrap'>
+        <div className='footer1'>
+          <a href='/'>
+            SAMSUNG Headset ,Ambrane Headset ,E-calorie Headset ,Philips Headset
+            ,FD1 Headset ,DIZO Headset ,TCL Headset ,Bluetooth & Wired Headset
+            ,Sennheiser Headset ,Skullcandy Headset ,WeCool Headset ,True
+            Wireless Headset ,ROQ Headset ,Apple Headset ,Wefuse Headset ,realme
+            Headset ,Toshiba Headset ,Motorola Headset ,Grostar Headset ,SYARA
+            Headset
+          </a>
+          <h2>Buy Headphones Online at Flipkart</h2>
+          &nbsp; &nbsp;
+          <p>
+            Headphones have become a must-have gadget in today's world owing to
+            the need of listening to music . It is important to buy these
+            headphones from a reliable online store as there are too many
+            defective and duplicate products being sold. Known for customer
+            friendly policies, Flipkart is one website that you can rely on to
+            buy the best headphones.
             <br></br>
-            
-Check out new launches: 
-            </p>
-      
-            <p></p>
-  
-
+            <br></br>
+            Check out new launches:
+            <a href='/'>
+              truke b097 bluetooth headset, truke deep bass, truke B120, truke
+              b120 price
+            </a>
+          </p>
+          <p>
+            Also Check:
+            <a href='/'>
+              Noise Tune Active Plus, Noise Tune Active Plus bluetooth, noise
+              tune elite sport bluetooth, noise tune elite sport,{' '}
+            </a>
+          </p>
+          <p>
+            Mivi Brand:
+            <a href='/'>Mivi Bluetooth, Mivi Duopods, mivi duopods m40, </a>
+          </p>
+          <p>
+            Intex Brand: 
+            <a href="/">Intex BT MUSIQUE Pro, Intex BT MUSIQUE BASS, bt musique bass, Intex MUSIQUE POWER, Intex MUSIQUE Flexi, Intex ROAR 101</a>
+          </p>
         </div>
       </div>
     </div>
--- src/components/footer/footer2/Footer2.tsx
+++ src/components/footer/footer2/Footer2.tsx
@@ -2,7 +2,9 @@
 const Footer2 = () => {
   return (
     <div>
-      
+      <div className="footer2-con">
+
+      </div>
     </div>
   )
 }
