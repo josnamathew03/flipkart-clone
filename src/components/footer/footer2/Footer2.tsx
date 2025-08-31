edf9f4d9b662f6eab5ad5a592b79f3d6851a87d4
Josna Mathew <150210764+josnamathew03@users.noreply.github.com>
2025-09-01 00:25:28 +0530

    feat: footer2 contents

--- src/components/footer/footer2/Footer2.tsx
+++ src/components/footer/footer2/Footer2.tsx
@@ -1,10 +1,46 @@
-
 const Footer2 = () => {
   return (
     <div>
-      <div className="footer2-con">
+      <div className='footer2-con'>
+        <div className='footer2'>
+          <div className='footer2-head'>ABOUT</div>
+          <a href='/'>Contact Us</a>
+          <a href='/'>About Us</a>
+          <a href='/'>Careers</a>
+          <a href='/'>Flipkart Stories</a>
+          <a href='/'>Press</a>
+          <a href='/'>Corporate Information</a>
+        </div>
+        <div className='footer2'>
+          <div className='footer2-head'>GROUP COMPANIES</div>
+          <a href='/'>Myntra</a>
+          <a href='/'>Cleartrip</a>
+          <a href='/'>Shopsy</a>
+        </div>
+        <div className='footer2'>
+          <div className='footer2-head'>HELP</div>
+          <a href='/'>Payments</a>
+          <a href='/'>Shipping</a>
+          <a href='/'>Cancellation & Returns</a>
+          <a href='/'>FAQ</a>
+        </div>
+        <div className='footer2'>
+          <div className='footer2-head'>CONSUMER POLICY</div>
+          <a href='/'>Cancellation & Returns</a>
+          <a href='/'>Terms Of Use</a>
+          <a href='/'>Security</a>
+          <a href='/'>Privacy</a>
+          <a href='/'>Sitemap</a>
+          <a href='/'>Grievance Redressal</a>
+          <a href='/'>EPR Compliance</a>
+        </div>
+        <div className='footer2'>
+          <div className="">
 
+          </div>
+        </div>
       </div>
+      <div className='footer2-foot'></div>
     </div>
   )
 }
