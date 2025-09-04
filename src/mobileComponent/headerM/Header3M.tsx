8315e2b0fcd5d289af9486cc95b4da85b6865f40
Josna Mathew <150210764+josnamathew03@users.noreply.github.com>
2025-09-04 14:31:12 +0530

    feat: header 3 added

--- src/mobileComponent/headerM/Header3M.tsx
+++ src/mobileComponent/headerM/Header3M.tsx
@@ -2,25 +2,31 @@ import Header3Spec from './Header3Spec'
 
 const Header3M = () => {
   return (
-    <div className='header3-m'>
-      <div className='offer-nav-wrap'>
-        <div className='offer-nav'>
-          <div className='offer-icon'>
-            <img
-              src='https://rukminim2.flixcart.com/www/64/64/promos/31/05/2023/a076c5a0-3335-4979-908e-49224c2507ea.png?q=60'
-              alt=''
-            />
-          </div>
-          <div className='offer-text-m-wrap'>
-            <div className='offer-text-m'>
-              <span>50% or <br /> more </span>
+
+          <div className='header3-m common'>
+            <div className='header3-m-wrap common'>
+              <div className='offer-nav-first'>
+                      <div className='offer-nav common '>
+                        <div className='offer-icon'>
+                          <img
+                            src='https://rukminim2.flixcart.com/www/64/64/promos/31/05/2023/a076c5a0-3335-4979-908e-49224c2507ea.png?q=60'
+                            alt=''
+                          />
+                        </div>
+                        <div className='common'>
+                          <div className='offer-text-m offerr'>
+                            <span>50% or more</span>
+                          </div>
+                    </div>
+                  </div>
+              </div>
+
+              <Header3Spec className='blue-back'>Bluetooth</Header3Spec>
+              <Header3Spec>Fast Charging</Header3Spec>
+              <Header3Spec>Noise Cancellation</Header3Spec>
             </div>
           </div>
-        </div>
-      </div>
-
-      <Header3Spec />
-    </div>
+  
   )
 }
 
--- src/mobileComponent/headerM/Header3Spec.tsx
+++ src/mobileComponent/headerM/Header3Spec.tsx
@@ -1,8 +1,23 @@
-
-const Header3Spec = () => {
+type Header3SpecProp = {
+  className?: string
+  children: string
+}
+const Header3Spec = ({ className, children }: Header3SpecProp) => {
   return (
-    <div>
-      
+    <div className='offer-nav-first'>
+      <div className={`offer-nav common ${className}`}>
+        <div className='offer-icon'>
+          <img
+            src='https://rukminim2.flixcart.com/www/32/32/promos/06/09/2022/ecf39e45-c8f0-4976-bbd3-43145e35d030.png?q=80'
+            alt=''
+          />
+        </div>
+        <div className='common'>
+          <div className='offer-text-m'>
+            <span>{children}</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
