152634fccb13e42e0ac3b6dd435f1f3a7675a8af
Josna Mathew <150210764+josnamathew03@users.noreply.github.com>
2025-09-08 15:48:40 +0530

    feat:  wish list toggle added

--- src/components/body/mainbody/Card.tsx
+++ src/components/body/mainbody/Card.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useRef } from 'react'
 import { productType } from '../../../types/productType'
 import { WishlistContext } from '../../../context/wishlist/WishlistProvider'
 
@@ -17,10 +17,15 @@ const Card = ({
 }: productType) => {
   const dis = Math.floor(((original - price) / original) * 100)
   const { dispatch } = useContext(WishlistContext)
+  const btnRef = useRef<SVGSVGElement  | null>(null)
 
-  const toggleWishlist = (e:React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const toggleWishlist = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.preventDefault()
-    dispatch({ type: 'addItem', item: { id, image, name } })
+    if (btnRef.current) btnRef.current.classList.toggle('like-svg')
+      btnRef.current?.classList.contains('like-svg') ? 
+    dispatch({ type: 'addItem', item: { id, image, name } }):
+    dispatch({ type: 'removeItem',  id })
+
   }
 
   return (
@@ -33,6 +38,7 @@ const Card = ({
           <div className='like-btn-con' onClick={e => toggleWishlist(e)}>
             <div className='like-btn'>
               <svg
+                ref={btnRef}
                 xmlns='http://www.w3.org/2000/svg'
                 width='16'
                 height='16'
@@ -40,7 +46,6 @@ const Card = ({
               >
                 <path
                   d='M8.695 16.682C4.06 12.382 1 9.536 1 6.065 1 3.219 3.178 1 5.95 1c1.566 0 3.069.746 4.05 1.915C10.981 1.745 12.484 1 14.05 1 16.822 1 19 3.22 19 6.065c0 3.471-3.06 6.316-7.695 10.617L10 17.897l-1.305-1.215z'
-                  fill='#e0e0e1'
                   stroke='#FFF'
                   fill-rule='evenodd'
                   opacity='.9'
